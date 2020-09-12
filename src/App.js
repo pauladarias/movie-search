f461822f52dd08fa46ad45895e8f891960353057
pauladarias <pauladarias@gmail.com>
2020-09-12 10:04:35 +0100

    Add some styling

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">React Movie Search</h1>
+    </div>
+  );
+}
+
+export default App;
