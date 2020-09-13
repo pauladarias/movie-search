4c46a9f8160a5ba938f02aeb43d768efd0e4c577
pauladarias <pauladarias@gmail.com>
2020-09-13 10:55:37 +0100

    Add state for query and movies

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SearchMovies() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (e) => {
     e.preventDefault();
-    console.log("submitting");
 
-    const query = "Jurassic Park";
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=7e8859b384ed2c19b2a4adcfe7f44364&language=en-US&query=${query}&page=1&include_adult=false`;
 
-    const url = `https://api.themoviedb.org/3/movie/550?api_key=7e8859b384ed2c19b2a4adcfe7f44364&language=en-US&query=${query}&page=1&include_adult=false`;
+    // const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
+      console.log(data.results);
+      setMovies(data.results);
     } catch (err) {
       console.error(err);
     }
@@ -30,6 +33,8 @@ function SearchMovies() {
           type="text"
           name="query"
           placeholder="i.e. Star Wars"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
         <button className="button" type="submit">
           Search
